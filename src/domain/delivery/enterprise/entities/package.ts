32ac88fe45071c69728adacedac7e350d2091ee7
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-19 10:52:03 -0300

    feat: create package entity

---
+++ src/domain/delivery/enterprise/entities/package.ts
@@ -0,0 +1,83 @@
+import { AggregateRoot } from '@/core/entities/aggregate-root';
+import type { UniqueEntityId } from '@/core/entities/value-object/unique-entity-id';
+import { PackageCode } from '../value-object/package-code';
+import { PackageStatus } from '../value-object/package-status';
+
+export interface PackageProps {
+  id: UniqueEntityId;
+  code: PackageCode;
+  recipientName: string;
+  recipientAddress: string;
+  deliveryPersonId: UniqueEntityId | null;
+  status: PackageStatus;
+  createdAt: Date;
+  updatedAt: Date | null;
+  deliveredAt: Date | null;
+}
+
+export class Package extends AggregateRoot<PackageProps> {
+  get id() {
+    return this.props.id;
+  }
+
+  get code() {
+    return this.props.code;
+  }
+
+  get recipientName() {
+    return this.props.recipientName;
+  }
+
+  get recipientAddress() {
+    return this.props.recipientAddress;
+  }
+
+  get deliveryPersonId() {
+    return this.props.deliveryPersonId;
+  }
+
+  get status() {
+    return this.props.status;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get deliveredAt() {
+    return this.props.deliveredAt;
+  }
+
+  public updateStatus(newStatus: PackageStatus): void {
+    this.props.status = this.props.status.transitionTo(newStatus);
+    this.props.updatedAt = new Date();
+
+    if (newStatus.isDelivered()) {
+      this.props.deliveredAt = new Date();
+    }
+  }
+
+  public assignDeliveryPerson(deliveryPersonId: UniqueEntityId): void {
+    this.props.deliveryPersonId = deliveryPersonId;
+    this.props.updatedAt = new Date();
+  }
+
+  public static create(props: PackageProps, id?: UniqueEntityId) {
+    return new Package(
+      {
+        ...props,
+        code: props.code ?? PackageCode.generate(),
+        status: props.status ?? PackageStatus.create('pending'),
+        deliveryPersonId: props.deliveryPersonId ?? null,
+        createdAt: props.createdAt ?? new Date(),
+        updatedAt: null,
+        deliveredAt: null,
+      },
+      id
+    );
+  }
+}
