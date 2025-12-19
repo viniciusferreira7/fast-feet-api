7e4a116074221e138ef43552b4751ff90206e47a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-19 09:59:17 -0300

    feat: create delivery person entity

---
+++ src/domain/delivery/enterprise/entities/delivery-person.ts
@@ -0,0 +1,42 @@
+import { AggregateRoot } from '@/core/entities/aggregate-root';
+import type { UniqueEntityId } from '@/core/entities/value-object/unique-entity-id';
+
+export interface DeliveryPersonProps {
+  id: UniqueEntityId;
+  name: string;
+  cpf: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date | null;
+}
+
+export class DeliveryPerson extends AggregateRoot<DeliveryPersonProps> {
+  get id() {
+    return this.props.id;
+  }
+  get name() {
+    return this.props.name;
+  }
+  get cpf() {
+    return this.props.cpf;
+  }
+  get password() {
+    return this.props.password;
+  }
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  public static create(props: DeliveryPersonProps, id?: UniqueEntityId) {
+    return new DeliveryPerson(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    );
+  }
+}
