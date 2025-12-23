a444e7954e857046646f323aca42067f29d9a54e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-23 12:12:22 -0300

    chore: run lint

--- src/core/either.spec.ts
+++ src/core/either.spec.ts
@@ -1,23 +1,23 @@
-import { type Either, left, right } from "./either";
+import { type Either, left, right } from './either';
 
 function doSomething(shouldBeSuccess: boolean): Either<string, string> {
-	if (!shouldBeSuccess) {
-		return left("error");
-	}
+  if (!shouldBeSuccess) {
+    return left('error');
+  }
 
-	return right("success");
+  return right('success');
 }
 
-test("error result", () => {
-	const result = doSomething(false);
+test('error result', () => {
+  const result = doSomething(false);
 
-	expect(result.isLeft()).toBeTruthy();
-	expect(result.isRight()).toBeFalsy();
+  expect(result.isLeft()).toBeTruthy();
+  expect(result.isRight()).toBeFalsy();
 });
 
-test("success result", () => {
-	const result = doSomething(true);
+test('success result', () => {
+  const result = doSomething(true);
 
-	expect(result.isRight()).toBeTruthy();
-	expect(result.isLeft()).toBeFalsy();
+  expect(result.isRight()).toBeTruthy();
+  expect(result.isLeft()).toBeFalsy();
 });
