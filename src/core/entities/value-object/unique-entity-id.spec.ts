d1157b2033048758d373893340f639bfc2e10c97
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-23 11:11:40 -0300

    chore: run lint command

--- src/core/entities/value-object/unique-entity-id.spec.ts
+++ src/core/entities/value-object/unique-entity-id.spec.ts
@@ -1,30 +1,30 @@
-import { UniqueEntityId } from './unique-entity-id'
+import { UniqueEntityId } from './unique-entity-id';
 
 describe('Unique entity ID', () => {
   it('should be able to create id if value doest not passed into constructor', () => {
-      const value = new UniqueEntityId()
+    const value = new UniqueEntityId();
 
-      expect(value).toBeInstanceOf(UniqueEntityId)
-  })
+    expect(value).toBeInstanceOf(UniqueEntityId);
+  });
 
   it('should be able to pass value into constructor', () => {
-      const value = new UniqueEntityId('value-id')
+    const value = new UniqueEntityId('value-id');
 
-      expect(value).toBeInstanceOf(UniqueEntityId)
-  })
+    expect(value).toBeInstanceOf(UniqueEntityId);
+  });
 
   it('should be able to return value from class', () => {
-      const value1 = new UniqueEntityId()
-      const value2 = new UniqueEntityId('value-id')
+    const value1 = new UniqueEntityId();
+    const value2 = new UniqueEntityId('value-id');
 
-      expect(value1.toString()).toEqual(expect.any(String))
-      expect(value2.toString()).toEqual('value-id')
-  })
+    expect(value1.toString()).toEqual(expect.any(String));
+    expect(value2.toString()).toEqual('value-id');
+  });
 
   it('should be able to compare different values', () => {
-      const value1 = new UniqueEntityId()
-      const value2 = new UniqueEntityId('value-id')
+    const value1 = new UniqueEntityId();
+    const value2 = new UniqueEntityId('value-id');
 
-      expect(value1.equals(value2)).toBeFalsy()
-  })
-})
+    expect(value1.equals(value2)).toBeFalsy();
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,16 @@
-import swc from "unplugin-swc";
-import tsConfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	test: {
-		globals: true,
-		root: "./",
-	},
-	plugins: [
-		tsConfigPaths(),
-		swc.vite({
-			module: { type: "es6" },
-		}),
-	],
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
 });
