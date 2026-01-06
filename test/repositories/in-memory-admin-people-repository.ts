82e6575e0ea473ec3cf7b91cfcd40301a968d957
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-06 10:36:27 -0300

    feat: create in-memory admin people repository

---
+++ test/repositories/in-memory-admin-people-repository.ts
@@ -0,0 +1,27 @@
+import type { AdminPeopleRepository } from '@/domain/delivery/application/repositories/admin-people-repository';
+import type { AdminPerson } from '@/domain/delivery/enterprise/entities/admin-person';
+
+export class InMemoryAdminPeopleRepository implements AdminPeopleRepository {
+  public adminPeople: AdminPerson[] = [];
+
+  async register(data: AdminPerson): Promise<AdminPerson> {
+    this.adminPeople.push(data);
+    return data;
+  }
+
+  async findByCpf(cpf: string): Promise<AdminPerson | null> {
+    const adminPerson = this.adminPeople.find(
+      (person) => person.cpf.value === cpf
+    );
+
+    return adminPerson ?? null;
+  }
+
+  async findByEmail(email: string): Promise<AdminPerson | null> {
+    const adminPerson = this.adminPeople.find(
+      (person) => person.email === email
+    );
+
+    return adminPerson ?? null;
+  }
+}
