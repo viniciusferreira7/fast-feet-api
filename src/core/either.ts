58b4db08566b2ce66f3fce05eeb2a550e90699cd
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-06 09:38:11 -0300

    chore: add JSDoc into either file

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,4 +1,8 @@
-// Error
+/**
+ * Represents the left side of an Either type, typically used for error values.
+ * @template L The type of the left (error) value
+ * @template R The type of the right (success) value
+ */
 export class Left<L, R> {
   readonly value: L;
 
@@ -6,16 +10,28 @@ export class Left<L, R> {
     this.value = value;
   }
 
+  /**
+   * Type guard to check if this is a Left instance.
+   * @returns true
+   */
   isLeft(): this is Left<L, R> {
     return true;
   }
 
+  /**
+   * Type guard to check if this is a Right instance.
+   * @returns false
+   */
   isRight(): this is Right<L, R> {
     return false;
   }
 }
 
-// Success
+/**
+ * Represents the right side of an Either type, typically used for success values.
+ * @template L The type of the left (error) value
+ * @template R The type of the right (success) value
+ */
 export class Right<L, R> {
   readonly value: R;
 
@@ -23,21 +39,49 @@ export class Right<L, R> {
     this.value = value;
   }
 
+  /**
+   * Type guard to check if this is a Left instance.
+   * @returns false
+   */
   isLeft(): this is Left<L, R> {
     return false;
   }
 
+  /**
+   * Type guard to check if this is a Right instance.
+   * @returns true
+   */
   isRight(): this is Right<L, R> {
     return true;
   }
 }
 
+/**
+ * Either type represents a value that can be either Left (error) or Right (success).
+ * This is a functional programming pattern for handling errors without exceptions.
+ * @template L The type of the left (error) value
+ * @template R The type of the right (success) value
+ */
 export type Either<L, R> = Left<L, R> | Right<L, R>;
 
+/**
+ * Creates a Left instance, typically used to represent an error or failure case.
+ * @template L The type of the left (error) value
+ * @template R The type of the right (success) value
+ * @param value The error value to wrap
+ * @returns An Either instance containing the left value
+ */
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left<L, R>(value);
 };
 
+/**
+ * Creates a Right instance, typically used to represent a success case.
+ * @template L The type of the left (error) value
+ * @template R The type of the right (success) value
+ * @param value The success value to wrap
+ * @returns An Either instance containing the right value
+ */
 export const right = <L, R>(value: R): Either<L, R> => {
   return new Right<L, R>(value);
 };
