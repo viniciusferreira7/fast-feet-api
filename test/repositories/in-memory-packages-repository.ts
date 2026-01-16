86d35f2fe9eb9abc812ccee621fcc42004d2da1c
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-16 10:57:02 -0300

    feat: return history of packge

--- test/repositories/in-memory-packages-repository.ts
+++ test/repositories/in-memory-packages-repository.ts
@@ -2,9 +2,14 @@ import { UniqueEntityId } from '@/core/entities/value-object/unique-entity-id';
 import { DomainEvents } from '@/core/events/domain-events';
 import type { PackagesRepository } from '@/domain/delivery/application/repositories/packages-repository';
 import type { Package } from '@/domain/delivery/enterprise/entities/package';
+import type { InMemoryPackagesHistoryRepository } from './in-memory-packages-history-repository';
 
 export class InMemoryPackagesRepository implements PackagesRepository {
   public packages: Package[] = [];
+
+  constructor(
+    private readonly packagesHistoryRepository: InMemoryPackagesHistoryRepository
+  ) {}
 
   async register(data: Package): Promise<Package> {
     this.packages.push(data);
@@ -18,7 +23,15 @@ export class InMemoryPackagesRepository implements PackagesRepository {
       pkg.id.equals(new UniqueEntityId(id))
     );
 
-    return packageItem ?? null;
+    if (!packageItem) return null;
+
+    this.packagesHistoryRepository.packagesHistory.forEach((history) => {
+      if (history.packageId.equals(new UniqueEntityId(id))) {
+        packageItem?.histories.add(history);
+      }
+    });
+
+    return packageItem;
   }
 
   async update(data: Package): Promise<Package | null> {
@@ -31,6 +44,12 @@ export class InMemoryPackagesRepository implements PackagesRepository {
     }
 
     this.packages[packageIndex] = data;
+
+    this.packagesHistoryRepository.packagesHistory.forEach((history) => {
+      if (history.packageId.equals(data.id)) {
+        data?.histories.add(history);
+      }
+    });
 
     DomainEvents.dispatchEventsForEntity(data.id);
 
