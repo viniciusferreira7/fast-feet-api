760a8ef343ac2115c3577b1b24bf9896c5901173
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-20 09:59:23 -0300

    feat: create notification entity and repository

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,50 @@
+import { Entity } from "@/core/entities/entity";
+import type { UniqueEntityId } from "@/core/entities/value-object/unique-entity-id";
+import type { Optional } from "@/core/types/optional";
+
+export interface NotificationProps {
+	recipientId: UniqueEntityId;
+	title: string;
+	content: string;
+	readAt?: Date | null;
+	createdAt: Date;
+}
+
+export class Notification extends Entity<NotificationProps> {
+	get recipientId() {
+		return this.props.recipientId;
+	}
+
+	get title() {
+		return this.props.title;
+	}
+
+	get content() {
+		return this.props.content;
+	}
+
+	get readAt() {
+		return this.props.readAt;
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	read() {
+		this.props.readAt = new Date();
+	}
+
+	static create(
+		props: Optional<NotificationProps, "createdAt">,
+		id?: UniqueEntityId,
+	) {
+		return new Notification(
+			{
+				...props,
+				createdAt: props.createdAt ?? new Date(),
+			},
+			id,
+		);
+	}
+}
