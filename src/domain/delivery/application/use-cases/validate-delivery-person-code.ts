f002a48795255f3e16d9c41b3b88257c73a9ade9
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-13 09:43:45 -0300

    feat: create use case to validate delivery person code

--- src/domain/delivery/application/use-cases/validate-delivery-person-code.ts
+++ src/domain/delivery/application/use-cases/validate-delivery-person-code.ts
@@ -1,3 +1,54 @@
-//confirm-delivery-identity.ts
-//TODO: This use case is to validate code
-//TODO: External e-mail service to confirm identity
+import { type Either, left, right } from '@/core/either';
+import type { DeliveryPerson } from '../../enterprise/entities/delivery-person';
+import { EmailCodeExpiredError } from '../../errors/email-code-expired-error';
+import { InvalidEmailCodeError } from '../../errors/invalid-email-code-error';
+import type { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
+import { EmailCodeHasNotBeenVerifiedError } from './errors/email-code-has-not-been-verified-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface ValidDeliveryPersonUseCaseRequest {
+  code: string;
+  email: string;
+}
+
+type ValidDeliveryPersonUseCaseResponse = Either<
+  | ResourceNotFoundError
+  | EmailCodeExpiredError
+  | InvalidEmailCodeError
+  | EmailCodeHasNotBeenVerifiedError,
+  { deliveryPerson: DeliveryPerson }
+>;
+
+export class ValidDeliveryPersonUseCase {
+  constructor(
+    private readonly deliveryPeopleRepository: DeliveryPeopleRepository
+  ) {}
+
+  async execute({
+    code,
+    email,
+  }: ValidDeliveryPersonUseCaseRequest): Promise<ValidDeliveryPersonUseCaseResponse> {
+    const deliveryPerson =
+      await this.deliveryPeopleRepository.findByEmail(email);
+
+    if (!deliveryPerson) {
+      return left(new ResourceNotFoundError('delivery'));
+    }
+
+    if (deliveryPerson.emailVerification?.isCodeExpired()) {
+      return left(new EmailCodeExpiredError());
+    }
+
+    if (!deliveryPerson.emailVerification?.validateCode(code)) {
+      return left(new InvalidEmailCodeError());
+    }
+
+    if (!deliveryPerson.isEmailValidated) {
+      return left(new EmailCodeHasNotBeenVerifiedError());
+    }
+
+    await this.deliveryPeopleRepository.update(deliveryPerson);
+
+    return right({ deliveryPerson });
+  }
+}
