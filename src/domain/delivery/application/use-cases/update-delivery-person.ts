d1428cead86988f4043106e2a301ee36d38a4b63
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-20 09:28:46 -0300

    feat: block disabled delivery person from updating profile

--- src/domain/delivery/application/use-cases/update-delivery-person.ts
+++ src/domain/delivery/application/use-cases/update-delivery-person.ts
@@ -3,6 +3,7 @@ import type { DeliveryPerson } from '../../enterprise/entities/delivery-person';
 import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
 import type { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
 import type { RecipientPeopleRepository } from '../repositories/recipient-people-repository';
+import { DeliveryPersonProfileIsDisableError } from './errors/delivery-person-profile-is-disable-error';
 import { EmailAlreadyInUseError } from './errors/email-already-in-use-error';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
@@ -13,7 +14,9 @@ interface UpdateDeliveryPersonUseCaseRequest {
 }
 
 type UpdateDeliveryPersonUseCaseResponse = Either<
-  ResourceNotFoundError | EmailAlreadyInUseError,
+  | ResourceNotFoundError
+  | EmailAlreadyInUseError
+  | DeliveryPersonProfileIsDisableError,
   { deliveryPerson: DeliveryPerson }
 >;
 
@@ -33,6 +36,10 @@ export class UpdateDeliveryPersonUseCase {
 
     if (!deliveryPerson) {
       return left(new ResourceNotFoundError('delivery'));
+    }
+
+    if (!deliveryPerson?.isActive) {
+      return left(new DeliveryPersonProfileIsDisableError());
     }
 
     if (email) {
