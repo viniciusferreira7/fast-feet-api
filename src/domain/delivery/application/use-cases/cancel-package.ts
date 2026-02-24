75178d7249940832c3dbe2f4e9115f319a18b018
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-24 08:42:04 -0300

    feat: implement cancel package use case

--- src/domain/delivery/application/use-cases/cancel-package.ts
+++ src/domain/delivery/application/use-cases/cancel-package.ts
@@ -1,2 +1,71 @@
 //TODO: create specific use cases to cancel
 //TODO: create use case to update package status
+
+import { type Either, left, right } from '@/core/either';
+import type { Package } from '../../enterprise/entities/package';
+import { InvalidatePackageStatusError } from '../../errors/invalidate-package-status-error';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import type { PackagesRepository } from '../repositories/packages-repository';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { PackageAlreadyCanceledErrorError } from './errors/package-already-canceled-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface CancelPackageUseCaseRequest {
+  authorId: string;
+  packageId: string;
+  description?: string;
+}
+
+type CancelPackageUseCaseResponse = Either<
+  | ResourceNotFoundError
+  | OnlyAdminCanPerformThisActionError
+  | PackageAlreadyCanceledErrorError
+  | InvalidatePackageStatusError,
+  { package: Package }
+>;
+
+export class CancelPackageUseCase {
+  constructor(
+    private readonly packagesRepository: PackagesRepository,
+    private readonly adminPeopleRepository: AdminPeopleRepository
+  ) {}
+
+  async execute({
+    authorId,
+    packageId,
+    description,
+  }: CancelPackageUseCaseRequest): Promise<CancelPackageUseCaseResponse> {
+    const [adminPerson, packageRegister] = await Promise.all([
+      this.adminPeopleRepository.findById(authorId),
+
+      this.packagesRepository.findById(packageId),
+    ]);
+
+    if (!adminPerson) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    if (!packageRegister) {
+      return left(new ResourceNotFoundError('package'));
+    }
+
+    const isPackageAlreadyCanceled = packageRegister.status.isCanceled();
+
+    if (isPackageAlreadyCanceled) {
+      return left(new PackageAlreadyCanceledErrorError());
+    }
+
+    const cancelPackageResult = packageRegister.cancelPackage(
+      adminPerson.id,
+      description
+    );
+
+    if (cancelPackageResult.isLeft()) {
+      return left(new InvalidatePackageStatusError());
+    }
+
+    await this.packagesRepository.update(packageRegister);
+
+    return right({ package: packageRegister });
+  }
+}
