da1a50370ca50b9fc32d8015ee3758169f1c1ce2
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-01 07:01:01 -0300

    feat: add pagination params interface

--- src/domain/delivery/application/repositories/packages-repository.ts
+++ src/domain/delivery/application/repositories/packages-repository.ts
@@ -2,9 +2,15 @@ import type { Pagination } from '@/core/entities/value-object/pagination';
 import type { Package } from '../../enterprise/entities/package';
 import type { PostalCode } from '../../enterprise/entities/value-object/postal-code';
 
+export interface FIndNearByProps {
+  postalCode: PostalCode;
+  page?: number;
+  perPage?: number;
+}
+
 export abstract class PackagesRepository {
   abstract register(data: Package): Promise<Package>;
   abstract findById(id: string): Promise<Package | null>;
   abstract update(data: Package): Promise<Package | null>;
-  abstract findNearBy(postCode: PostalCode): Promise<Pagination<Package>>;
+  abstract findNearBy(props: FIndNearByProps): Promise<Pagination<Package>>;
 }
