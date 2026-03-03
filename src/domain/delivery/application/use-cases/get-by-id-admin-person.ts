bb3652f18c002d1b050f81a388d9e435548990bf
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-03 09:34:01 -0300

    feat: create get by id use cases for admin and recipient person

---
+++ src/domain/delivery/application/use-cases/get-by-id-admin-person.ts
@@ -0,0 +1,43 @@
+import { type Either, left, right } from '@/core/either';
+import type { AdminPerson } from '../../enterprise/entities/admin-person';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface GetByIdAdminPersonUseCaseRequest {
+  adminPersonId: string;
+  authorId: string;
+}
+
+type GetByIdAdminPersonUseCaseResponse = Either<
+  ResourceNotFoundError | OnlyAdminCanPerformThisActionError,
+  { adminPerson: AdminPerson }
+>;
+
+export class GetByIdAdminPersonUseCase {
+  constructor(private readonly adminPeopleRepository: AdminPeopleRepository) {}
+
+  async execute({
+    adminPersonId,
+    authorId,
+  }: GetByIdAdminPersonUseCaseRequest): Promise<GetByIdAdminPersonUseCaseResponse> {
+    const isSamePersonLookingForThisPerson = adminPersonId === authorId;
+
+    const [adminPerson, author] = await Promise.all([
+      this.adminPeopleRepository.findById(adminPersonId),
+      !isSamePersonLookingForThisPerson
+        ? this.adminPeopleRepository.findById(authorId)
+        : Promise.resolve(null),
+    ]);
+
+    if (!adminPerson) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (!isSamePersonLookingForThisPerson && !author) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    return right({ adminPerson });
+  }
+}
---
+++ src/domain/delivery/application/use-cases/get-by-id-recipient-person.ts
@@ -0,0 +1,47 @@
+import { type Either, left, right } from '@/core/either';
+import type { RecipientPerson } from '../../enterprise/entities/recipient-person';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import type { RecipientPeopleRepository } from '../repositories/recipient-people-repository';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface GetByIdRecipientPersonUseCaseRequest {
+  recipientPersonId: string;
+  authorId: string;
+}
+
+type GetByIdRecipientPersonUseCaseResponse = Either<
+  ResourceNotFoundError | OnlyAdminCanPerformThisActionError,
+  { recipientPerson: RecipientPerson }
+>;
+
+export class GetByIdRecipientPersonUseCase {
+  constructor(
+    private readonly recipientPeopleRepository: RecipientPeopleRepository,
+    private readonly adminPeopleRepository: AdminPeopleRepository
+  ) {}
+
+  async execute({
+    recipientPersonId,
+    authorId,
+  }: GetByIdRecipientPersonUseCaseRequest): Promise<GetByIdRecipientPersonUseCaseResponse> {
+    const isSamePersonLookingForThisPerson = recipientPersonId === authorId;
+
+    const [recipientPerson, author] = await Promise.all([
+      this.recipientPeopleRepository.findById(recipientPersonId),
+      !isSamePersonLookingForThisPerson
+        ? this.adminPeopleRepository.findById(authorId)
+        : Promise.resolve(null),
+    ]);
+
+    if (!recipientPerson) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (!isSamePersonLookingForThisPerson && !author) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    return right({ recipientPerson });
+  }
+}
