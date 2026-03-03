2cf1d76c65ed02234feb65124316c0c83c97d6ab
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-03 09:34:20 -0300

    test: create unit tests for get by id admin and recipient person use cases

---
+++ src/domain/delivery/application/use-cases/get-by-id-admin-person.spec.ts
@@ -0,0 +1,82 @@
+import { makeAdminPerson } from 'test/factories/make-admin-person';
+import { InMemoryAdminPeopleRepository } from 'test/repositories/in-memory-admin-people-repository';
+import { AdminPerson } from '../../enterprise/entities/admin-person';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { GetByIdAdminPersonUseCase } from './get-by-id-admin-person';
+
+let adminPeopleRepository: InMemoryAdminPeopleRepository;
+let sut: GetByIdAdminPersonUseCase;
+
+describe('Get By Id Admin Person', () => {
+  beforeEach(() => {
+    adminPeopleRepository = new InMemoryAdminPeopleRepository();
+    sut = new GetByIdAdminPersonUseCase(adminPeopleRepository);
+  });
+
+  it('should be able to get admin person info as another admin', async () => {
+    const author = makeAdminPerson();
+    const adminPerson = makeAdminPerson();
+
+    await adminPeopleRepository.register(author);
+    await adminPeopleRepository.register(adminPerson);
+
+    const result = await sut.execute({
+      adminPersonId: adminPerson.id.toString(),
+      authorId: author.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      expect(result.value.adminPerson).toBeInstanceOf(AdminPerson);
+      expect(result.value.adminPerson.id.equals(adminPerson.id)).toBe(true);
+    }
+  });
+
+  it('should be able to get own admin person info', async () => {
+    const adminPerson = makeAdminPerson();
+
+    await adminPeopleRepository.register(adminPerson);
+
+    const result = await sut.execute({
+      adminPersonId: adminPerson.id.toString(),
+      authorId: adminPerson.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      expect(result.value.adminPerson).toBeInstanceOf(AdminPerson);
+      expect(result.value.adminPerson.id.equals(adminPerson.id)).toBe(true);
+    }
+  });
+
+  it('should not be able to get admin person info with non-existent admin person', async () => {
+    const author = makeAdminPerson();
+    await adminPeopleRepository.register(author);
+
+    const result = await sut.execute({
+      adminPersonId: 'non-existent-admin-person-id',
+      authorId: author.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+    }
+  });
+
+  it('should not be able to get admin person info with non-existent author', async () => {
+    const adminPerson = makeAdminPerson();
+    await adminPeopleRepository.register(adminPerson);
+
+    const result = await sut.execute({
+      adminPersonId: adminPerson.id.toString(),
+      authorId: 'non-existent-author-id',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(OnlyAdminCanPerformThisActionError);
+    }
+  });
+});
---
+++ src/domain/delivery/application/use-cases/get-by-id-recipient-person.spec.ts
@@ -0,0 +1,111 @@
+import { makeAdminPerson } from 'test/factories/make-admin-person';
+import { makeRecipientPerson } from 'test/factories/make-recipient-person';
+import { InMemoryAdminPeopleRepository } from 'test/repositories/in-memory-admin-people-repository';
+import { InMemoryRecipientPeopleRepository } from 'test/repositories/in-memory-recipient-people-repository';
+import { RecipientPerson } from '../../enterprise/entities/recipient-person';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { GetByIdRecipientPersonUseCase } from './get-by-id-recipient-person';
+
+let adminPeopleRepository: InMemoryAdminPeopleRepository;
+let recipientPeopleRepository: InMemoryRecipientPeopleRepository;
+let sut: GetByIdRecipientPersonUseCase;
+
+describe('Get By Id Recipient Person', () => {
+  beforeEach(() => {
+    adminPeopleRepository = new InMemoryAdminPeopleRepository();
+    recipientPeopleRepository = new InMemoryRecipientPeopleRepository();
+    sut = new GetByIdRecipientPersonUseCase(
+      recipientPeopleRepository,
+      adminPeopleRepository
+    );
+  });
+
+  it('should be able to get recipient person info as an admin', async () => {
+    const admin = makeAdminPerson();
+    const recipientPerson = makeRecipientPerson();
+
+    await adminPeopleRepository.register(admin);
+    await recipientPeopleRepository.register(recipientPerson);
+
+    const result = await sut.execute({
+      recipientPersonId: recipientPerson.id.toString(),
+      authorId: admin.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      expect(result.value.recipientPerson).toBeInstanceOf(RecipientPerson);
+      expect(result.value.recipientPerson.id.equals(recipientPerson.id)).toBe(
+        true
+      );
+    }
+  });
+
+  it('should be able to get own recipient person info', async () => {
+    const recipientPerson = makeRecipientPerson();
+
+    await recipientPeopleRepository.register(recipientPerson);
+
+    const result = await sut.execute({
+      recipientPersonId: recipientPerson.id.toString(),
+      authorId: recipientPerson.id.toString(),
+    });
+
+    expect(result.isRight()).toBe(true);
+    if (result.isRight()) {
+      expect(result.value.recipientPerson).toBeInstanceOf(RecipientPerson);
+      expect(result.value.recipientPerson.id.equals(recipientPerson.id)).toBe(
+        true
+      );
+    }
+  });
+
+  it('should not be able to get recipient person info with non-existent recipient person', async () => {
+    const admin = makeAdminPerson();
+    await adminPeopleRepository.register(admin);
+
+    const result = await sut.execute({
+      recipientPersonId: 'non-existent-recipient-person-id',
+      authorId: admin.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(ResourceNotFoundError);
+    }
+  });
+
+  it('should not be able to get recipient person info with non-admin author', async () => {
+    const recipientPerson1 = makeRecipientPerson();
+    const recipientPerson2 = makeRecipientPerson();
+
+    await recipientPeopleRepository.register(recipientPerson1);
+    await recipientPeopleRepository.register(recipientPerson2);
+
+    const result = await sut.execute({
+      recipientPersonId: recipientPerson1.id.toString(),
+      authorId: recipientPerson2.id.toString(),
+    });
+
+    expect(result.isLeft()).toBe(true);
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(OnlyAdminCanPerformThisActionError);
+    }
+  });
+
+  it('should not be able to get recipient person info with non-existent author', async () => {
+    const recipientPerson = makeRecipientPerson();
+    await recipientPeopleRepository.register(recipientPerson);
+
+    const result = await sut.execute({
+      recipientPersonId: recipientPerson.id.toString(),
+      authorId: 'non-existent-author-id',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    if (result.isLeft()) {
+      expect(result.value).toBeInstanceOf(OnlyAdminCanPerformThisActionError);
+    }
+  });
+});
