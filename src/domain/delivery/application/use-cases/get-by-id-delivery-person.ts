494bec34f586c3c52f1c96a2184df9e736abd4d1
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-03 09:25:41 -0300

    feat: create get by id delivery person use case

---
+++ src/domain/delivery/application/use-cases/get-by-id-delivery-person.ts
@@ -0,0 +1,47 @@
+import { type Either, left, right } from '@/core/either';
+import type { DeliveryPerson } from '../../enterprise/entities/delivery-person';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import type { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface GetByIdDeliveryPersonUseCaseRequest {
+  deliveryPersonId: string;
+  authorId: string;
+}
+
+type GetByIdDeliveryPersonUseCaseResponse = Either<
+  ResourceNotFoundError | OnlyAdminCanPerformThisActionError,
+  { deliveryPerson: DeliveryPerson }
+>;
+
+export class GetByIdDeliveryPersonUseCase {
+  constructor(
+    private readonly deliveryPeopleRepository: DeliveryPeopleRepository,
+    private readonly adminPeopleRepository: AdminPeopleRepository
+  ) {}
+
+  async execute({
+    deliveryPersonId,
+    authorId,
+  }: GetByIdDeliveryPersonUseCaseRequest): Promise<GetByIdDeliveryPersonUseCaseResponse> {
+    const isSamePersonLookingForThisPerson = deliveryPersonId === authorId;
+
+    const [deliveryPerson, author] = await Promise.all([
+      this.deliveryPeopleRepository.findById(deliveryPersonId),
+      !isSamePersonLookingForThisPerson
+        ? this.adminPeopleRepository.findById(authorId)
+        : Promise.resolve(null),
+    ]);
+
+    if (!deliveryPerson) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (!isSamePersonLookingForThisPerson && !author) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    return right({ deliveryPerson });
+  }
+}
