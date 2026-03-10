c88a04f935a8a013bd9608739537a6d7d6d5a304
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-10 09:39:15 -0300

    test: create unit tests for on package picked up notification subscriber

---
+++ src/domain/notification/subscribers/on-package-picked-up-send-notification.spec.ts
@@ -0,0 +1,99 @@
+import { makeDeliveryPerson } from 'test/factories/make-delivery-person';
+import { makePackage } from 'test/factories/make-package';
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository';
+import { InMemoryPackagesHistoryRepository } from 'test/repositories/in-memory-packages-history-repository';
+import { InMemoryPackagesRepository } from 'test/repositories/in-memory-packages-repository';
+import { waitFor } from 'test/utils/wait-for';
+import { beforeEach, MockInstance } from 'vitest';
+import { PackageStatus } from '@/domain/delivery/enterprise/entities/value-object/package-status';
+import {
+  SendNotificationUseCase,
+  SendNotificationUseCaseRequest,
+  SendNotificationUseCaseResponse,
+} from '../application/use-cases/send-notification';
+import { OnPackagePickedUpSendNotification } from './on-package-picked-up-send-notification';
+
+let packagesRepository: InMemoryPackagesRepository;
+let packagesHistoryRepository: InMemoryPackagesHistoryRepository;
+let notificationsRepository: InMemoryNotificationsRepository;
+
+let sendNotificationUseCase: SendNotificationUseCase;
+
+let sendNotificationSpy: MockInstance<
+  (
+    request: SendNotificationUseCaseRequest
+  ) => Promise<SendNotificationUseCaseResponse>
+>;
+
+describe('On package picked up', () => {
+  beforeEach(() => {
+    packagesHistoryRepository = new InMemoryPackagesHistoryRepository();
+    packagesRepository = new InMemoryPackagesRepository(
+      packagesHistoryRepository
+    );
+
+    notificationsRepository = new InMemoryNotificationsRepository();
+
+    sendNotificationUseCase = new SendNotificationUseCase(
+      notificationsRepository
+    );
+
+    sendNotificationSpy = vi.spyOn(sendNotificationUseCase, 'execute');
+
+    const _onPackagePickedUpSendNotification =
+      new OnPackagePickedUpSendNotification(
+        packagesRepository,
+        sendNotificationUseCase
+      );
+  });
+
+  it('should send notification when a package is picked up', async () => {
+    const deliveryPerson = makeDeliveryPerson();
+    const awaitingPickupStatus = PackageStatus.create('awaiting_pickup');
+
+    if (awaitingPickupStatus.isLeft()) {
+      throw new Error('Failed to create awaiting_pickup status');
+    }
+
+    const packageRecord = makePackage({
+      status: awaitingPickupStatus.value,
+      deliveryPersonId: deliveryPerson.id,
+    });
+
+    await packagesRepository.register(packageRecord);
+
+    packageRecord.markAsPickedUp(deliveryPerson.id);
+
+    await packagesRepository.update(packageRecord);
+
+    await waitFor(() => {
+      expect(sendNotificationSpy).toBeCalledTimes(1);
+      expect(sendNotificationSpy).toBeCalledWith({
+        title: 'Package picked up',
+        content: `Delivery person was picked up to get a package: ${
+          packageRecord.name.length > 10
+            ? packageRecord.name.substring(0, 10).concat('...')
+            : packageRecord.name
+        }, the package code is: ${packageRecord.code.value}`,
+        recipientId: packageRecord.recipientId.toString(),
+      });
+    });
+
+    const notification = notificationsRepository.items.find(
+      (item) => item.title === 'Package picked up'
+    );
+
+    expect(notification).toEqual(
+      expect.objectContaining({
+        title: 'Package picked up',
+        content: `Delivery person was picked up to get a package: ${
+          packageRecord.name.length > 10
+            ? packageRecord.name.substring(0, 10).concat('...')
+            : packageRecord.name
+        }, the package code is: ${packageRecord.code.value}`,
+        recipientId: packageRecord.recipientId,
+        createdAt: expect.any(Date),
+      })
+    );
+  });
+});
