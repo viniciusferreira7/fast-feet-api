268129c78156ad823cd2b92d9c17fae59eca2d89
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-11 08:45:23 -0300

    feat: add on package at distribution center notification subscriber

---
+++ src/domain/notification/subscribers/on-package-is-at-a-distribution-center-send-notification.ts
@@ -0,0 +1,46 @@
+import { DomainEvents } from '@/core/events/domain-events';
+import type { EventHandler } from '@/core/events/event-handler';
+import type { PackagesRepository } from '@/domain/delivery/application/repositories/packages-repository';
+import { PackageAssignedToADeliveryPersonEvent } from '@/domain/delivery/enterprise/events/package-assigned-to-a-delivery-person-event';
+import type { SendNotificationUseCase } from '../application/use-cases/send-notification';
+
+export class OnPackageIsAtADistributionCenterSendNotification
+  implements EventHandler
+{
+  constructor(
+    private readonly packagesRepository: PackagesRepository,
+    private readonly sendNotificationUseCase: SendNotificationUseCase
+  ) {
+    this.setupSubscriptions();
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendNewPackageNotification.bind(this),
+      PackageAssignedToADeliveryPersonEvent.name
+    );
+  }
+
+  private async sendNewPackageNotification({
+    packageHistory,
+    packageId,
+  }: PackageAssignedToADeliveryPersonEvent) {
+    const packageRecord = await this.packagesRepository.findById(
+      packageId.toString()
+    );
+
+    if (!packageRecord) return;
+
+    await this.sendNotificationUseCase.execute({
+      title:
+        packageHistory.description ??
+        'Package arrived at a distribution center',
+      content: `Your package ${
+        packageRecord.name.length > 10
+          ? packageRecord.name.substring(0, 10).concat('...')
+          : packageRecord.name
+      } has arrived at a distribution center. The package code is: ${packageRecord.code.value}`,
+      recipientId: packageRecord.recipientId.toString(),
+    });
+  }
+}
