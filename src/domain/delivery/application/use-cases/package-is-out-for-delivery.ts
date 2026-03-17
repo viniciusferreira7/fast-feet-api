8307b0136affd21381daa4394fc1d07a68a74428
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-17 09:01:08 -0300

    feat: create PackageIsOutForDelivery use case

---
+++ src/domain/delivery/application/use-cases/package-is-out-for-delivery.ts
@@ -0,0 +1,70 @@
+import { type Either, left, right } from '@/core/either';
+import type { Package } from '../../enterprise/entities/package';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import type { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
+import type { PackagesRepository } from '../repositories/packages-repository';
+import { InvalidatePackageStatusError } from '../../errors/invalidate-package-status-error';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface PackageIsOutForDeliveryUseCaseRequest {
+  authorId: string;
+  deliveryPersonId: string;
+  packageId: string;
+  description?: string;
+}
+
+type PackageIsOutForDeliveryUseCaseResponse = Either<
+  | ResourceNotFoundError
+  | InvalidatePackageStatusError
+  | OnlyAdminCanPerformThisActionError,
+  { package: Package }
+>;
+
+export class PackageIsOutForDeliveryUseCase {
+  constructor(
+    private readonly adminPeopleRepository: AdminPeopleRepository,
+    private readonly deliveryPeopleRepository: DeliveryPeopleRepository,
+    private readonly packagesRepository: PackagesRepository
+  ) {}
+
+  async execute({
+    authorId,
+    deliveryPersonId,
+    packageId,
+    description,
+  }: PackageIsOutForDeliveryUseCaseRequest): Promise<PackageIsOutForDeliveryUseCaseResponse> {
+    const [adminPersonRecord, deliveryPersonRecord, packageRecord] =
+      await Promise.all([
+        this.adminPeopleRepository.findById(authorId),
+        this.deliveryPeopleRepository.findById(deliveryPersonId),
+        this.packagesRepository.findById(packageId),
+      ]);
+
+    if (!adminPersonRecord) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    if (!deliveryPersonRecord) {
+      return left(new ResourceNotFoundError('delivery person'));
+    }
+
+    if (!packageRecord) {
+      return left(new ResourceNotFoundError('package'));
+    }
+
+    const transitionResult = packageRecord.markAsOutForDelivery(
+      adminPersonRecord.id,
+      deliveryPersonRecord.id,
+      description
+    );
+
+    if (transitionResult.isLeft()) {
+      return left(transitionResult.value);
+    }
+
+    await this.packagesRepository.update(packageRecord);
+
+    return right({ package: packageRecord });
+  }
+}
