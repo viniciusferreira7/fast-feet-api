5bcd58543e1f937d54772d074c5379856c031441
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-20 10:29:11 -0300

    fix: correct ResourceNotFoundError message from 'recipient' to 'package' in UpdatePackage

---
+++ src/domain/delivery/application/use-cases/update-package.ts
@@ -0,0 +1,62 @@
+import { type Either, left, right } from '@/core/either';
+import { Package } from '../../enterprise/entities/package';
+import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import type { PackagesRepository } from '../repositories/packages-repository';
+import { OnlyAdminCanPerformThisActionError } from './errors/only-admin-can-perform-this-action-error';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+
+interface UpdatePackageUseCaseRequest {
+  name: string;
+  recipientAddress: string;
+  authorId: string;
+  packageId: string;
+  description?: string;
+}
+
+type UpdatePackageUseCaseResponse = Either<
+  ResourceNotFoundError | OnlyAdminCanPerformThisActionError,
+  {
+    package: Package;
+  }
+>;
+
+export class UpdatePackage {
+  constructor(
+    private readonly packagesRepository: PackagesRepository,
+    private readonly adminPeopleRepository: AdminPeopleRepository
+  ) {}
+
+  async execute({
+    name,
+    authorId,
+    recipientAddress,
+    packageId,
+    description,
+  }: UpdatePackageUseCaseRequest): Promise<UpdatePackageUseCaseResponse> {
+    const [author, packageRecord] = await Promise.all([
+      this.adminPeopleRepository.findById(authorId),
+
+      this.packagesRepository.findById(packageId),
+    ]);
+
+    if (!author) {
+      return left(new OnlyAdminCanPerformThisActionError());
+    }
+
+    if (!packageRecord) {
+      return left(new ResourceNotFoundError('package'));
+    }
+
+    packageRecord.update(
+      {
+        name,
+        recipientAddress,
+      },
+      description
+    );
+
+    await this.packagesRepository.update(packageRecord);
+
+    return right({ package: packageRecord });
+  }
+}
