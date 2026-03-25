7c298aa76ea67ee971e015689ea18ba522a565b4
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-25 08:16:55 -0300

    feat: add MarkAsReadNotificationUseCase

---
+++ src/domain/notification/application/use-cases/mark-as-read-notification.ts
@@ -0,0 +1,40 @@
+import { type Either, left, right } from '@/core/either';
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
+import type { Notification } from '../../enterprise/entities/notification';
+import type { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface MarkAsReadNotificationUseCaseRequest {
+  authorId: string;
+  notificationId: string;
+}
+
+type MarkAsReadNotificationUseCaseResponse = Either<
+  ResourceNotFoundError,
+  { notification: Notification }
+>;
+
+export class MarkAsReadNotificationUseCase {
+  constructor(
+    private readonly notificationsRepository: NotificationsRepository
+  ) {}
+
+  async execute({
+    authorId,
+    notificationId,
+  }: MarkAsReadNotificationUseCaseRequest): Promise<MarkAsReadNotificationUseCaseResponse> {
+    const notification = await this.notificationsRepository.findByIdAndAuthorId(
+      authorId,
+      notificationId
+    );
+
+    if (!notification) {
+      return left(new ResourceNotFoundError('notification'));
+    }
+
+    notification.read();
+
+    await this.notificationsRepository.save(notification);
+
+    return right({ notification });
+  }
+}
