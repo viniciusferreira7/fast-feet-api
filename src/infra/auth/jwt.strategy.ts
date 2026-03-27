786165fa9e70c3d6dcee1223dbfc57ad704d5193
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-27 09:25:44 -0300

    chore: run lint command

--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -1,29 +1,30 @@
-import { Injectable } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { z } from "zod";
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { z } from 'zod';
 
-import { EnvService } from "../env/env.service";
+import { EnvService } from '../env/env.service';
 
 const tokenPayloadSchema = z.object({
-	sub: z.string().uuid(),
+  sub: z.string().uuid(),
 });
 
 export type UserPayload = z.infer<typeof tokenPayloadSchema>;
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly env: EnvService) {
-		const publicKey = env.get("JWT_PUBLIC_KEY");
+  // biome-ignore lint/correctness/noUnusedPrivateClassMembers: Cannot use this before calling super
+  constructor(private readonly env: EnvService) {
+    const publicKey = env.get('JWT_PUBLIC_KEY');
 
-		super({
-			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-			secretOrKey: Buffer.from(publicKey, "base64"),
-			algorithms: ["RS256"],
-		});
-	}
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: Buffer.from(publicKey, 'base64'),
+      algorithms: ['RS256'],
+    });
+  }
 
-	async validate(payload: UserPayload) {
-		return tokenPayloadSchema.parse(payload);
-	}
+  async validate(payload: UserPayload) {
+    return tokenPayloadSchema.parse(payload);
+  }
 }
