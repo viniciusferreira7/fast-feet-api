6793fc347bd766cf699c2f47fa971da258589920
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-27 08:30:12 -0300

    chore: register CryptographyModule in AppModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CryptographyModule } from './cryptography/cryptography.module';
 
 @Module({
-  imports: [],
+  imports: [CryptographyModule],
   controllers: [AppController],
   providers: [AppService],
 })
