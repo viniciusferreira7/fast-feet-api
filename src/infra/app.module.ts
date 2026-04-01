043531cafd99c9be3920ae9cc358c2f8e5f72b9e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-01 08:16:10 -0300

    fix: add AuthModule, EnvModule and ConfigModule to AppModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { CryptographyModule } from './cryptography/cryptography.module';
 import { EmailModule } from './email/email.module';
+import { envSchema } from './env/env';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [CryptographyModule, EmailModule],
+  imports: [
+    AuthModule,
+    CryptographyModule,
+    EmailModule,
+    EnvModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.test', '.env'],
+      validate: (env) => {
+        return envSchema.parse(env);
+      },
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
