f6b95c4484baf51b94dd465aca0f5cf545a4ccc2
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-01 08:17:08 -0300

    chore: add vitest config for e2e tests

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,23 @@
+import swc from 'unplugin-swc';
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
+    testTimeout: 1_000_000,
+    hookTimeout: 1_000_000,
+  },
+  optimizeDeps: {
+    exclude: ['generated/prisma'],
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+});
