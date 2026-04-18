98c9b7f54b630f87fa00b1ac31d7fc6c31c74f46
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-18 12:36:19 -0300

    fix(test): correct envs

--- test/setup-for-infra-tests.ts
+++ test/setup-for-infra-tests.ts
@@ -18,11 +18,9 @@ let cleanupDb: ReturnType<typeof drizzle>;
 beforeAll(async () => {
   DomainEvents.shouldRun = false;
 
-  const connectionString = process.env.CI
-  ? `postgresql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@localhost:5432/${process.env.DATABASE_NAME}`
-  : env.DATABASE_URL;
+  const connectionString = env.DATABASE_URL;
 
-cleanupPool = new Pool({ connectionString });
+  cleanupPool = new Pool({ connectionString });
   cleanupDb = drizzle(cleanupPool);
 
   if (process.env.CI) {
