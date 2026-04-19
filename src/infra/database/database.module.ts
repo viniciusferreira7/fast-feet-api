ee2e036279a5883a19943f4090f753f77d952776
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-19 09:51:51 -0300

    feat(database): register DrizzleRecipientPeopleRepository in database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminPeopleRepository } from '@/domain/delivery/application/repositories/admin-people-repository';
 import { DeliveryPeopleRepository } from '@/domain/delivery/application/repositories/delivery-people-repository';
+import { RecipientPeopleRepository } from '@/domain/delivery/application/repositories/recipient-people-repository';
 import { DrizzleService } from './drizzle/drizzle.service';
 import { DrizzleAdminPeopleRepository } from './drizzle/repositories/drizzle-admin-people-repository';
 import { DrizzleDeliveryPeopleRepository } from './drizzle/repositories/drizzle-delivery-people-repository';
+import { DrizzleRecipientPeopleRepository } from './drizzle/repositories/drizzle-recipient-people-repository';
 
 @Module({
   providers: [
@@ -16,7 +18,16 @@ import { DrizzleDeliveryPeopleRepository } from './drizzle/repositories/drizzle-
       provide: DeliveryPeopleRepository,
       useClass: DrizzleDeliveryPeopleRepository,
     },
+    {
+      provide: RecipientPeopleRepository,
+      useClass: DrizzleRecipientPeopleRepository,
+    },
   ],
-  exports: [DrizzleService, AdminPeopleRepository, DeliveryPeopleRepository],
+  exports: [
+    DrizzleService,
+    AdminPeopleRepository,
+    DeliveryPeopleRepository,
+    RecipientPeopleRepository,
+  ],
 })
 export class DatabaseModule {}
