428160c9a82fbc14fa89bab418bb13222e8e18e5
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-21 13:20:07 -0300

    refactor(decorators): rename files to set decorator suffix

--- src/infra/auth/guards/jwt-auth.guard.ts
+++ src/infra/auth/guards/jwt-auth.guard.ts
@@ -2,7 +2,7 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { log } from '../../logger';
-import { IS_PUBLIC_KEY } from '../decorators/public';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
