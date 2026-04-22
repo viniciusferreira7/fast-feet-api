13152b3e4f0d5d2970f62dc45891748501db2028
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-22 09:11:45 -0300

    chore(decorators): move current user decorator file to decorators folder

--- src/infra/auth/decorators/current-user.decorator.ts
+++ src/infra/auth/decorators/current-user.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, type ExecutionContext } from '@nestjs/common';
-
-import type { UserPayload } from './jwt.strategy';
+import type { UserPayload } from '../jwt.strategy';
 
 export const CurrentUser = createParamDecorator(
   (_: never, context: ExecutionContext) => {
