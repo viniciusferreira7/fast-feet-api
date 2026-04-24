a7fa9ef287a1cadbcc65f093de146c8dce3724b6
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-24 11:37:18 -0300

    feat(presenters): create admin person presenter

---
+++ src/infra/http/presenters/admin-person-presenter.ts
@@ -0,0 +1,23 @@
+import type { AdminPerson } from '@/domain/delivery/enterprise/entities/admin-person';
+
+interface AdminPersonPresenterToHttp {
+  id: string;
+  name: string;
+  cpf: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date | null;
+}
+
+export class AdminPersonPresenter {
+  public static toHttp(adminPerson: AdminPerson): AdminPersonPresenterToHttp {
+    return {
+      id: adminPerson.id.toString(),
+      name: adminPerson.name,
+      cpf: adminPerson.cpf.value,
+      email: adminPerson.email,
+      created_at: adminPerson.createdAt,
+      updated_at: adminPerson.updatedAt,
+    };
+  }
+}
