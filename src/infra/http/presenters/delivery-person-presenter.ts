a8d347bc6a9cc8da3d39a1bb40a0aa045fcb0e9a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-24 11:45:37 -0300

    feat(presenters): create delivery person presenter

---
+++ src/infra/http/presenters/delivery-person-presenter.ts
@@ -0,0 +1,27 @@
+import type { DeliveryPerson } from '@/domain/delivery/enterprise/entities/delivery-person';
+
+interface DeliveryPersonPresenterToHttp {
+  id: string;
+  name: string;
+  cpf: string;
+  email: string;
+  is_active: boolean;
+  created_at: Date;
+  updated_at: Date | null;
+}
+
+export class DeliveryPersonPresenter {
+  public static toHttp(
+    deliveryPerson: DeliveryPerson
+  ): DeliveryPersonPresenterToHttp {
+    return {
+      id: deliveryPerson.id.toString(),
+      name: deliveryPerson.name,
+      cpf: deliveryPerson.cpf.value,
+      email: deliveryPerson.email,
+      is_active: deliveryPerson.isActive,
+      created_at: deliveryPerson.createdAt,
+      updated_at: deliveryPerson.updatedAt,
+    };
+  }
+}
