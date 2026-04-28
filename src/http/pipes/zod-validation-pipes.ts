10df377e51235a807ba5db6c35e0c8381432f7bc
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-28 10:55:49 -0300

    feat(pipes): create zod validation pipe to use on controllers

---
+++ src/http/pipes/zod-validation-pipes.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, type PipeTransform } from '@nestjs/common';
+import { ZodError, type ZodType } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private readonly schema: ZodType) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch (error: unknown) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed',
+          statusCode: 400,
+        });
+      }
+      throw new BadRequestException('Validation failed');
+    }
+  }
+}
