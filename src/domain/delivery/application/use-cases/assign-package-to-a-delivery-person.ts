8c45be22686cc7ba473164f84136b560da3b6260
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-01 12:41:34 -0300

    refactor(use-cases): convert type-only imports to concrete imports in AssignPackageToADeliveryPerson

--- src/domain/delivery/application/use-cases/assign-package-to-a-delivery-person.ts
+++ src/domain/delivery/application/use-cases/assign-package-to-a-delivery-person.ts
@@ -3,9 +3,9 @@ import { type Either, left, right } from '@/core/either';
 import { ResourceNotFoundError } from '../../../../core/errors/resource-not-found-error';
 import { Package } from '../../enterprise/entities/package';
 import { InvalidatePackageStatusError } from '../../errors/invalidate-package-status-error';
-import type { AdminPeopleRepository } from '../repositories/admin-people-repository';
-import type { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
-import type { PackagesRepository } from '../repositories/packages-repository';
+import { AdminPeopleRepository } from '../repositories/admin-people-repository';
+import { DeliveryPeopleRepository } from '../repositories/delivery-people-repository';
+import { PackagesRepository } from '../repositories/packages-repository';
 
 interface AssignPackageToADeliveryPersonUseCaseRequest {
   packageId: string;
