1dca0ceab7521c285e6159246030a88de2781524
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-01 12:41:42 -0300

    feat(http): register RegisterAdminPersonController in HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpClient } from '@/domain/delivery/application/http/http-client';
 import { AssignPackageToADeliveryPersonUseCase } from '@/domain/delivery/application/use-cases/assign-package-to-a-delivery-person';
 import { AssignPackageToADeliveryPersonController } from './controllers/assign-package-to-a-delivery-person.controller';
+import { RegisterAdminPersonController } from './controllers/register-admin-person.controller';
 import { FetchHttpClient } from './fetch-http-client';
 
 @Module({
@@ -13,6 +14,9 @@ import { FetchHttpClient } from './fetch-http-client';
     AssignPackageToADeliveryPersonUseCase,
   ],
   exports: [HttpClient],
-  controllers: [AssignPackageToADeliveryPersonController],
+  controllers: [
+    AssignPackageToADeliveryPersonController,
+    RegisterAdminPersonController,
+  ],
 })
 export class HttpModule {}
