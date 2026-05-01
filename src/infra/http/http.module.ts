3d90fc73e01bc483642b657c75bddff5fbc96d1d
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-01 13:18:25 -0300

    fix(http): import DatabaseModule, CryptographyModule and ValidationModule to resolve use case dependencies

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { HttpClient } from '@/domain/delivery/application/http/http-client';
 import { AssignPackageToADeliveryPersonUseCase } from '@/domain/delivery/application/use-cases/assign-package-to-a-delivery-person';
 import { RegisterAdminPersonUseCase } from '@/domain/delivery/application/use-cases/register-admin-person';
+import { CryptographyModule } from '@/infra/cryptography/cryptography.module';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { ValidationModule } from '../validation/validation.module';
 import { AssignPackageToADeliveryPersonController } from './controllers/assign-package-to-a-delivery-person.controller';
 import { RegisterAdminPersonController } from './controllers/register-admin-person.controller';
 import { FetchHttpClient } from './fetch-http-client';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    CryptographyModule,
+    forwardRef(() => ValidationModule),
+  ],
   providers: [
     {
       provide: HttpClient,
--- src/infra/validation/validation.module.ts
+++ src/infra/validation/validation.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { PasswordValidator } from '@/domain/delivery/application/validation/password-validator';
 import { HttpModule } from '@/infra/http/http.module';
 import { PasswordService } from './password.service';
 import { PostalCodeService } from './postal-code.service';
 
 @Module({
-  imports: [HttpModule],
-  providers: [PasswordService, PostalCodeService],
+  imports: [forwardRef(() => HttpModule)],
+  providers: [
+    PasswordService,
+    PostalCodeService,
+    { provide: PasswordValidator, useClass: PasswordService },
+  ],
+  exports: [PasswordValidator],
 })
 export class ValidationModule {}
