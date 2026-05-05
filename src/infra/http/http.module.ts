b4ab14c7bc763438c98f04b2da554e1e5fc79e51
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-05 09:43:43 -0300

    feat(http): register SendAdminPersonCodeUseCase and EmailModule in HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,18 +3,22 @@ import { HttpClient } from '@/domain/delivery/application/http/http-client';
 import { AssignPackageToADeliveryPersonUseCase } from '@/domain/delivery/application/use-cases/assign-package-to-a-delivery-person';
 import { AuthenticateAdminPersonUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin-person';
 import { RegisterAdminPersonUseCase } from '@/domain/delivery/application/use-cases/register-admin-person';
+import { SendAdminPersonCodeUseCase } from '@/domain/delivery/application/use-cases/send-admin-person-code';
 import { RoleGuard } from '@/infra/auth/guards/role.guard';
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module';
 import { DatabaseModule } from '@/infra/database/database.module';
+import { EmailModule } from '@/infra/email/email.module';
 import { ValidationModule } from '../validation/validation.module';
 import { AssignPackageToADeliveryPersonController } from './controllers/assign-package-to-a-delivery-person.controller';
 import { AuthenticateAdminPersonController } from './controllers/authenticate-admin-person.controller';
 import { RegisterAdminPersonController } from './controllers/register-admin-person.controller';
+import { SendAdminPersonCodeController } from './controllers/send-admin-person-code.controller';
 import { FetchHttpClient } from './fetch-http-client';
 
 @Module({
   imports: [
     DatabaseModule,
+    EmailModule,
     CryptographyModule,
     forwardRef(() => ValidationModule),
   ],
@@ -27,12 +31,14 @@ import { FetchHttpClient } from './fetch-http-client';
     AssignPackageToADeliveryPersonUseCase,
     RegisterAdminPersonUseCase,
     AuthenticateAdminPersonUseCase,
+    SendAdminPersonCodeUseCase,
   ],
   exports: [HttpClient],
   controllers: [
     AssignPackageToADeliveryPersonController,
     RegisterAdminPersonController,
     AuthenticateAdminPersonController,
+    SendAdminPersonCodeController,
   ],
 })
 export class HttpModule {}
