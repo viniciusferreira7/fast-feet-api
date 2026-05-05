5b96288963382d4c432522398fe49d86da1d61b6
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-05 09:26:16 -0300

    feat(http): register AuthenticateAdminPersonUseCase and controller in HttpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { HttpClient } from '@/domain/delivery/application/http/http-client';
 import { AssignPackageToADeliveryPersonUseCase } from '@/domain/delivery/application/use-cases/assign-package-to-a-delivery-person';
+import { AuthenticateAdminPersonUseCase } from '@/domain/delivery/application/use-cases/authenticate-admin-person';
 import { RegisterAdminPersonUseCase } from '@/domain/delivery/application/use-cases/register-admin-person';
 import { RoleGuard } from '@/infra/auth/guards/role.guard';
 import { CryptographyModule } from '@/infra/cryptography/cryptography.module';
 import { DatabaseModule } from '@/infra/database/database.module';
 import { ValidationModule } from '../validation/validation.module';
 import { AssignPackageToADeliveryPersonController } from './controllers/assign-package-to-a-delivery-person.controller';
+import { AuthenticateAdminPersonController } from './controllers/authenticate-admin-person.controller';
 import { RegisterAdminPersonController } from './controllers/register-admin-person.controller';
 import { FetchHttpClient } from './fetch-http-client';
 
@@ -24,11 +26,13 @@ import { FetchHttpClient } from './fetch-http-client';
     RoleGuard,
     AssignPackageToADeliveryPersonUseCase,
     RegisterAdminPersonUseCase,
+    AuthenticateAdminPersonUseCase,
   ],
   exports: [HttpClient],
   controllers: [
     AssignPackageToADeliveryPersonController,
     RegisterAdminPersonController,
+    AuthenticateAdminPersonController,
   ],
 })
 export class HttpModule {}
